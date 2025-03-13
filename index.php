76645fad964f5e89578996cbcf6566868c464c6c
OnlineAquaCow <victor.thibodeau4k@gmail.com>
2025-03-13 09:24:49 -0400

    Remove temp tests from root index.php

--- index.php
+++ index.php
@@ -4,15 +4,4 @@ require 'src/functions.php';
 
 require "src/configuration.php";
 
-
-require 'src/class/Database.php';
-require 'models/ItemModel.php';
-
-$pdo = Database::getInstance()->getPDO();
-$itemModel = new ItemModel($pdo);
-
-$comment = $itemModel->selectById(2);
-
-$itemModel->createItem('itemName', 'itemDescription', 'itemWeight', 'itemPrice', 'itemSellPrice', 'itemImageLink', 'itemUtility', 'itemStatus');
-
 routeToController(uriPath());
