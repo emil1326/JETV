4c7616428a1213df94359b0337b7672225679114
ThomasAneakot <137071772+ThomasAneakot@users.noreply.github.com>
2025-03-24 15:09:50 -0400

    Update detailstemp.php

--- views/detailstemp.php
+++ views/detailstemp.php
@@ -8,14 +8,41 @@ require 'views/partials/header.php';
         display: none;
     }
 
-
-
-
-
     .colorStar {
 
         color: white;
 
+    }
+
+    .cardCar {
+        background-color: #303030;
+        color: white;
+        border: none;
+    }
+
+    icon-shape {
+        display: inline-flex;
+        align-items: center;
+        justify-content: center;
+        text-align: center;
+        vertical-align: middle;
+    }
+
+    .icon-sm {
+        width: 2rem;
+        height: 2rem;
+
+    }
+
+    input::-webkit-outer-spin-button,
+    input::-webkit-inner-spin-button {
+        -webkit-appearance: none;
+        margin: 0;
+    }
+
+
+    input[type="number"] {
+        -moz-appearance: textfield;
     }
 </style>
 <div style="display:flex; justify-content: center; flex-direction: column;align-items:center;">
@@ -25,7 +52,7 @@ require 'views/partials/header.php';
         <div class="row g-0">
             <div class="col-md-4" style="width:50%; max-height:325px; ">
                 <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ-YtnuV2n_8xuMZbIQ8voSyC4hjGBN6DLC8w&s"
-                    class="img-fluid rounded-start" alt="..." style="width:100%;height:100%;aspect-ratio: auto;">
+                    class="img-fluid rounded-start" alt="..." style="width:100%;height:110%;aspect-ratio: auto;">
             </div>
             <div class=" col-md-8" style="padding-left:30px; width:50%; max-height:325px; ">
                 <div class="card-body" style="max-height:325px !important;">
@@ -35,8 +62,9 @@ require 'views/partials/header.php';
                     <p class="card-text"><small class="text-muted">Lorem ipsum dolor sit, amet consectetur adipisicing
                             elit. Ea, maxime quos cumque dolorem ad</small></p>
                     <button class="btn btn-outline-secondary " type="button"
-                        style="margin-right:10px; width:100%;  border : none; border-radius:8px;padding-top:4px; background-color: #303030;"><a
-                            href="/login" class="buttonst " style="color:white;">Acheter</a>
+                        style="margin-right:10px; width:100%;  border : none; border-radius:8px;padding-top:4px; background-color: #303030;"
+                        data-bs-toggle="modal" data-bs-target="#exampleModalCenter"><a class="buttonst "
+                            style="color:white;">Acheter</a>
                     </button>
                     <div
                         style="display:flex; flex-direction: column; background-color: #303030; margin-top:10px; padding:15px; border-radius:8px; border:1px  #6c757d solid;">
@@ -61,6 +89,135 @@ require 'views/partials/header.php';
                     </div>
 
                 </div>
+            </div>
+        </div>
+    </div>
+    <div style="background-color: #303030; width:100%; margin-top:60px; padding-bottom:80px; margin-bottom:200px;">
+        <div class="container" style="margin-top:60px; background-color: #303030;">
+            <div class="row">
+                <div class="col-6">
+                    <h3 class="mb-3" style="font-size:20px;">Attributs</h3>
+                </div>
+                <div class="col-6 text-right">
+                    <a class="btn btn-secondary mb-3 mr-1" href="#carouselExampleIndicators2" role="button"
+                        data-slide="prev" style="background-color:#303030; border:none;">
+                        <i class="fa fa-arrow-left"></i>
+                    </a>
+                    <a class="btn btn-secondary mb-3" href="#carouselExampleIndicators2" role="button" data-slide="next"
+                        style="background-color:#303030; border:none;">
+                        <i class="fa fa-arrow-right"></i>
+                    </a>
+                </div>
+                <div class="col-12">
+                    <div id="carouselExampleIndicators2" class="carousel slide" data-ride="carousel">
+
+                        <div class="carousel-inner">
+
+                            <?php for ($i = 0; $i < 3; $i++): ?>
+                                <div class="carousel-item <?= $i == 0 ? 'active' : '' ?>">
+                                    <div class="row">
+
+                                        <!-- Faire en sorte que tu peut mettre trois chaque carousel-item mais pas plus, un ou deux est correct -->
+                                        <div class="col-md-4 mb-3">
+                                            <div class="card cardCar">
+
+                                                <div class="card-body">
+                                                    <h4 class="card-title">
+                                                        Special title treatment <?= $i ?></h4>
+                                                    <p class="card-text">With supporting text
+                                                        below as a natural lead-in</p>
+
+                                                </div>
+                                            </div>
+                                        </div>
+
+                                        <div class="col-md-4 mb-3">
+                                            <div class="card cardCar">
+
+                                                <div class="card-body">
+                                                    <h4 class="card-title">Special title
+                                                        treatment</h4>
+                                                    <p class="card-text">With supporting text
+                                                        below as a natural lead-in
+                                                    </p>
+
+                                                </div>
+                                            </div>
+                                        </div>
+
+                                        <div class="col-md-4 mb-3">
+                                            <div class="card cardCar">
+
+                                                <div class="card-body">
+                                                    <h4 class="card-title">Special title treatment</h4>
+                                                    <p class="card-text">With supporting text
+                                                        below as a natural lead-in</p>
+
+                                                </div>
+                                            </div>
+                                        </div>
+
+                                    </div>
+                                </div>
+                            <?php endfor; ?>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+    <div class="modal fade" id="exampleModalCenter" tabindex="-1" aria-labelledby="exampleModalCenterTitle"
+        aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered">
+            <div class="modal-content" style="background-color:#303030;">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalCenterTitle">Ajouter votre quantité</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <div class="col-12">
+                        <div class="d-flex justify-content-between">
+                            <div>
+                                <p style="color:white;">Sword 1 $777</p>
+                            </div>
+                            <div class="input-group w-auto justify-content-end align-items-center">
+                                <input type="button" value="-"
+                                    class="button-minus border rounded-circle  icon-shape icon-sm mx-1 "
+                                    data-field="quantity"
+                                    style="color:white; border:none; font-weight:bold;background-color: transparent;">
+                                <input type="number" step="1" max="10" value="1" name="quantity"
+                                    class="quantity-field border-0 text-center w-25"
+                                    style="background-color:transparent; color:white; font-size:20px;" disabled>
+                                <input type="button" value="+"
+                                    class="button-plus border rounded-circle icon-shape icon-sm "
+                                    style="color:white; border:none; font-weight:bold;background-color: transparent;"
+                                    data-field="quantity">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fermer</button>
+                    <button type="button" class="btn btn-primary" style="background-color: white; color:black;"
+                        id="liveToastBtn">Ajouter</button>
+
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class=" toast-container position-fixed p-3 top-0 start-50 translate-middle-x"
+        style="z-index: 11; margin-top:20px;">
+        <div id="liveToast" class="toast hide" role="alert" aria-live="assertive" aria-atomic="true">
+            <div class="toast-header">
+
+                <strong class="me-auto">Ajout du panier ✔</strong>
+                <small>just now</small>
+                <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
+            </div>
+            <div class="toast-body" style="background-color: white; color:black;">
+                L'ajout a été éffectué
             </div>
         </div>
     </div>
@@ -115,8 +272,55 @@ require 'views/partials/header.php';
         </div>
     </div> -->
 </div>
+<script>
 
-</div>
+    function incrementValue(e) {
+        e.preventDefault();
+        var fieldName = $(e.target).data('field');
+        var parent = $(e.target).closest('div');
+        var currentVal = parseInt(parent.find('input[name=' + fieldName + ']').val(), 10);
+
+        if (!isNaN(currentVal) && currentVal < 10) {
+            parent.find('input[name=' + fieldName + ']').val(currentVal + 1);
+        } else {
+            parent.find('input[name=' + fieldName + ']').val(1);
+        }
+    }
+
+    function decrementValue(e) {
+        e.preventDefault();
+        var fieldName = $(e.target).data('field');
+        var parent = $(e.target).closest('div');
+        var currentVal = parseInt(parent.find('input[name=' + fieldName + ']').val(), 10);
+
+        if (!isNaN(currentVal) && currentVal > 1) {
+            parent.find('input[name=' + fieldName + ']').val(currentVal - 1);
+        } else {
+            parent.find('input[name=' + fieldName + ']').val(1);
+        }
+    }
+
+    $('.input-group').on('click', '.button-plus', function (e) {
+        incrementValue(e);
+    });
+
+    $('.input-group').on('click', '.button-minus', function (e) {
+        decrementValue(e);
+    });
+
+</script>
+<script>
+    document.getElementById("liveToastBtn").onclick = function () {
+        var toastElList = [].slice.call(document.querySelectorAll('.toast'))
+        var toastList = toastElList.map(function (toastEl) {
+            return new bootstrap.Toast(toastEl)
+        })
+        toastList.forEach(toast => toast.show())
+    }
+</script>
+<script type="text/javascript" src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js"></script>
+<script type="text/javascript" src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js"></script>
 
 <?php
 require 'views/partials/footer.php';
