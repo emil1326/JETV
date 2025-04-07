12bd2e8bc56254b63e828c6f2c234c51513f94c0
OnlineAquaCow <victor.thibodeau4k@gmail.com>
2025-04-07 13:47:15 -0400

    Create enigmaQuestion page

---
+++ controllers/enigmaQuestion.php
@@ -0,0 +1,9 @@
+<?php
+
+if (!isAuthenticated()) {
+    echo 'DEV log: You are not logged in. You shouldn\'t be able to see this';
+    //redirect('/');
+}
+
+
+require 'views/enigmaQuestion.php';
--- models/QuestModel.php
+++ models/QuestModel.php
@@ -85,4 +85,3 @@ class Quest extends Model
         return null;
     }
 }
-
