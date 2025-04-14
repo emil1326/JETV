48528ca5a37602a471dd599ac5e58dffe66d1d7a
ThomasAneakot <137071772+ThomasAneakot@users.noreply.github.com>
2025-04-14 14:57:08 -0400

    q

--- views/shop.php
+++ views/shop.php
@@ -12,32 +12,212 @@ require 'views/partials/header.php';
         transition: all 0.5s ease;
     }
 
+    .card {
+        min-width: 300px !important;
+    }
+
     .card-group .card:hover {
         transform: scale(1.05);
         box-shadow: 0 0 3px white;
     }
 
-    @media (max-width: 576px) {
-        .card-group {
-            display: flex;
-            flex-flow: row wrap;
-        }
-    }
+
+
 
     .card-img-top {
         max-height: 300px;
         min-height: 250px;
         aspect-ratio: 2/3;
     }
+
+    .masonry {
+        display: grid;
+        grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+        grid-auto-rows: 45px;
+        gap: 5px;
+
+        width: 100%;
+        margin: 0 auto;
+    }
+
+    #allShop {
+        display: flex;
+        flex-direction: row;
+        column-gap: 20px;
+        justify-content: center;
+    }
+
+    #formCrit {
+        display: flex;
+        flex-direction: row;
+        column-gap: 20px;
+        justify-content: center;
+        align-items: center;
+    }
+
+    #form-check-section {
+        display: flex;
+        flex-direction: column;
+        width: 240px;
+        padding: 16px;
+        border: 1px #6c757d solid;
+        row-gap: 5px;
+        height: 425px;
+    }
+
+    #taggies {
+        display: flex;
+        flex-direction: row;
+        column-gap: 8px;
+        margin-left: 50px;
+    }
+
+    #shopitems {
+        justify-content: center;
+        width: 55%;
+
+    }
+
+
+    @media only screen and (max-width: 1200px) {
+        .masonry {
+            display: grid;
+            grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+            grid-auto-rows: 45px;
+            gap: 5px;
+            width: 100%;
+
+            align-items: center;
+            justify-content: center;
+
+            margin: 0 auto;
+
+        }
+
+        #allShop {
+            display: flex;
+            flex-direction: column;
+            column-gap: 20px;
+            row-gap: 20px;
+            justify-content: center;
+            align-items: center;
+        }
+
+        #formCrit {
+            display: flex;
+            flex-direction: row;
+            column-gap: 20px;
+            justify-content: center;
+            align-items: center;
+        }
+
+        #form-check-section {
+            display: flex;
+            flex-direction: column;
+            width: 100%;
+            padding: 16px;
+            border: 1px #6c757d solid;
+            row-gap: 5px;
+            height: 425px;
+        }
+
+        #main-form {
+            width: 50%;
+
+        }
+
+        #taggies {
+            display: flex;
+            flex-direction: row;
+            column-gap: 8px;
+            margin-left: 0px;
+        }
+    }
+
+    @media only screen and (max-width: 1000px) {
+        .masonry {
+            display: grid;
+            grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+            grid-auto-rows: 45px;
+            gap: 5px;
+            width: 100%;
+            align-content: center;
+            justify-content: center;
+
+            margin: 0 auto;
+        }
+
+        #allShop {
+            display: flex;
+            flex-direction: column;
+            column-gap: 20px;
+            row-gap: 20px;
+            justify-content: center;
+            align-items: center;
+        }
+
+        #formCrit {
+            display: flex;
+            flex-direction: row;
+            column-gap: 20px;
+            justify-content: center;
+            align-items: center;
+        }
+
+        #form-check-section {
+            display: flex;
+            flex-direction: column;
+            width: 100%;
+            padding: 16px;
+            border: 1px #6c757d solid;
+            row-gap: 5px;
+            height: 425px;
+        }
+
+        #main-form {
+            width: 50%;
+
+        }
+
+        #shopitems {
+            justify-content: center;
+            width: 90%;
+
+        }
+
+        #formCrit {
+            flex-direction: column;
+            row-gap: 10px;
+        }
+    }
+
+    .item {
+        width: 100%;
+        padding: 20px;
+        box-sizing: border-box;
+        box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+        border-radius: 8px;
+        transition: transform 0.3s;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        font-size: 1.2em;
+        color: #fff;
+        grid-row: span 10;
+        align-content: center;
+    }
+
+    .item:hover {
+        transform: translateY(-10px);
+    }
 </style>
-<div style="display:flex; flex-direction:row; column-gap: 20px;justify-content:center;">
+<div id="allShop">
 
     <form id='main-form' method='GET'>
 
         <!--  TODO: Add Name in form (search filters) -->
 
-        <div id="form-check-section"
-            style="display:flex; flex-direction: column; width:240px;padding:16px; border:1px #6c757d solid; row-gap: 5px; height:425px; ">
+        <div id="form-check-section">
 
             <p>Types</p>
             <div class="form-check">
@@ -80,15 +260,13 @@ require 'views/partials/header.php';
 
             <div data-mdb-input-init class="form-outline">
                 <label for="postfix" class="form-label" style="display: inline;">Price Range</label>
-                <input id="postfix"
-                    value="<?= $filters['price_min'] ?? '' ?>"
-                    type="number" id="form2" name="price_min" class="form-control" placeholder="Min" style="height:30px; margin-top: 5px;" />
+                <input id="postfix" value="<?= $filters['price_min'] ?? '' ?>" type="number" id="form2" name="price_min"
+                    class="form-control" placeholder="Min" style="height:30px; margin-top: 5px;" />
             </div>
 
             <div data-mdb-input-init class="form-outline">
-                <input id="postfix"
-                    value="<?= $filters['price_max'] ?? '' ?>"
-                    type="number" id="form2" name="price_max" class="form-control" placeholder="Max" style="height:30px;" />
+                <input id="postfix" value="<?= $filters['price_max'] ?? '' ?>" type="number" id="form2" name="price_max"
+                    class="form-control" placeholder="Max" style="height:30px;" />
             </div>
             <br>
 
@@ -97,14 +275,16 @@ require 'views/partials/header.php';
 
     </form>
 
-    <div style="display:flex; flex-direction: column; max-width:1000px; row-gap: 40px;">
-        <div style="display:flex; flex-direction: row; column-gap: 20px;  justify-content:center; align-items: center;">
+
+
+
+    <div id="shopitems" style="display:flex; flex-direction: column; max-width:1000px; row-gap: 40px;">
+        <div id="formCrit">
             <p style="margin-bottom:0px; margin-right:0px;"> Nom : </p>
             <div class="input-group rounded"
-                style="width:486px; background-color:#1E1E1E; border:1px  #6c757d solid; border-radius:100px !important; color:white;">
-                <input form='main-form' id="search" type="search" name='name' class="form-control rounded searchy" placeholder="Search"
-                    value="<?= $filters['name'] ?? '' ?>"
-                    aria-label="Search"
+                style="width:100%; background-color:#1E1E1E; border:1px  #6c757d solid; border-radius:100px !important; color:white;">
+                <input form='main-form' id="search" type="search" name='name' class="form-control rounded searchy"
+                    placeholder="Search" value="<?= $filters['name'] ?? '' ?>" aria-label="Search"
                     style="background-color:#1E1E1E; border:0px;border-radius:100px !important; color:white;"
                     aria-describedby="search-addon" />
                 <span class="input-group-text border-0" id="search-addon"
@@ -112,7 +292,7 @@ require 'views/partials/header.php';
                     <i class="fas fa-search"></i>
                 </span>
             </div>
-            <div style="display:flex; flex-direction: row; column-gap:8px; margin-left:50px;">
+            <div id="taggies">
 
                 <input type="radio" class="btn-check" name="options" id="option1" autocomplete="off" checked
                     style="border-radius:100px">
@@ -129,16 +309,16 @@ require 'views/partials/header.php';
             </div>
         </div>
 
-        <!-- Card -->
 
-        <div style="display:flex; justify-content: center; align-items: center; max-width:1000px">
-            <div class="card-group" style="display:flex; justify-content: center; row-gap: 3ch;">
-                <?php if (isset($items)): ?>
-                    <?php foreach ($items as $index => $item): ?>
+
+        <div class="masonry">
+            <?php if (isset($items)): ?>
+                <?php foreach ($items as $index => $item): ?>
+                    <div class="item buttonCursorPointer"
+                        onclick="window.location.href='/details?itemID=<?= $item['item']->getId() ?>'">
 
                         <div class="card"
-                            style="background-color:#1E1E1E !important; padding:10px; cursor:pointer;border:1px white solid;border-color: #6c757d; border-radius:8px; margin:20px; margin-top:0px; margin-bottom:0px;"
-                            onclick="window.location.href='/details?itemID=<?= $item['item']->getId() ?>'">
+                            style="background-color:#1E1E1E !important; padding:10px; cursor:pointer;border:1px white solid;border-color: #6c757d; border-radius:8px; margin:20px; margin-top:0px; margin-bottom:0px;">
                             <div class="numberCircle" style="margin-right:0px;"><?= $item['quantity'] ?></div>
                             <img class="card-img-top" src="public/images/<?= $item['item']->getImageLink() ?>"
                                 alt="Card image cap" style="background-color:white;">
@@ -153,14 +333,17 @@ require 'views/partials/header.php';
                             </div>
                         </div>
 
-                    <?php endforeach; ?>
-                <?php else: ?>
-                    <div style="margin-right: 20vw;"> <!-- todo justify center -->
-                        Rien a afficher ici <br>
+
+
                     </div>
-                <?php endif ?>
-            </div>
+                <?php endforeach; ?>
+            <?php else: ?>
+                <div style="margin-right: 20vw;"> <!-- todo justify center -->
+                    Rien a afficher ici <br>
+                </div>
+            <?php endif ?>
         </div>
+
     </div>
 </div>
 
