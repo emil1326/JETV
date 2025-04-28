5f2f60ce276ac064c6bb6b1bb76c56c10cacfce5
Basque Jonathan <202174101@edu.clg.qc.ca>
2025-04-28 15:03:30 -0400

    Profileform shows

--- controllers/profileForm.php
+++ controllers/profileForm.php
@@ -3,3 +3,36 @@ if (!isAuthenticated()) {
     echo 'DEV log: You are not logged in. You shouldn\'t be able to see this';
     redirect('/');
 }
+$pdo = Database::getInstance()->getPDO();
+$model = new UserModel($pdo);
+$user= $model->selectById($_SESSION['playerID']);
+$firstName = $_POST['firstName'] ?? $user->getFirstName();
+$lastName = $_POST['lastName'] ?? $user->getLastName();
+$username = $_POST['username'] ?? $user->getUsername();
+$password = $_POST['password'] ?? '';
+$passwordConfirm = $_POST['passwordConfirm'] ?? '';
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+    $pdo = Database::getInstance()->getPDO();
+    $userModel = new UserModel($pdo);
+
+    // Check for empty fields
+    if (emptyFields($firstName, $lastName, $username, $password, $passwordConfirm)) {
+        $messageKey = displayError('Tous les renseignements doivent être remplis');
+    } else if (strlen($password) < 8 && strlen($password) > 0) {
+        $messageKey = displayError('Le mot de passe dois être au moins 8 caractères');
+    } else if ($password != $passwordConfirm) {
+        $messageKey = displayError('Les deux mots de passe ne sont pas identiques');
+    } else if (!$userModel->isUsernameAvailable($username)) {
+        $messageKey = displayError('Cet alias est déjà pris');
+    } else {
+        if(!empty($password)) {
+            $password = password_hash($password, PASSWORD_DEFAULT);
+        } else {
+            $password = $user->getPassword();
+        }
+        //$userModel->updateUser($username, $firstName, $lastName, $password);
+        header("Location: /");
+    }
+}
+require 'views/profileForm.php';
