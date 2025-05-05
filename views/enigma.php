a2c60965f3ebbaee335066fbe0d0a0f3ddb23698
ThomasAneakot <137071772+ThomasAneakot@users.noreply.github.com>
2025-05-05 14:45:39 -0400

    a

--- views/enigma.php
+++ views/enigma.php
@@ -13,12 +13,15 @@ require 'partials/header.php';
             difficulté </h1>
 
         <div class="masonry">
-            <div class="item item1 buttonCursorPointer" onclick="window.location.href='/enigmaQuestion?difficulty=1'">Facile</div>
-            <div class="item item2 buttonCursorPointer" onclick="window.location.href='/enigmaQuestion?difficulty=2'">Moyen</div>
-            <div class="item item3 buttonCursorPointer" onclick="window.location.href='/enigmaQuestion?difficulty=3'">Difficile</div>
+            <div class="item item1 buttonCursorPointer" onclick="window.location.href='/enigmaQuestion?difficulty=1'">
+                Facile</div>
+            <div class="item item2 buttonCursorPointer" onclick="window.location.href='/enigmaQuestion?difficulty=2'">
+                Moyen</div>
+            <div class="item item3 buttonCursorPointer" onclick="window.location.href='/enigmaQuestion?difficulty=3'">
+                Difficile</div>
         </div>
         <div id="enigmaStrRetour" class="col-lg-6 mx-auto mb-3 text-center">
-            <a href="/" type="button" class="btn btn-secondary">Retours a l'Accueil</a>
+            <a href="/" type="button" class="btn btn-secondary">Retour à l'accueil</a>
         </div>
     </div>
 </div>
